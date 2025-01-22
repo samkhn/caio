ba39e65c7386ddb1d0e321d0a7ead5b81aaf5891
Samiur Khan <samiurkh1n@gmail.com>
2025-01-22 13:46:16 -0500

    add len + message protocol

--- examples/tcp_client.c
+++ examples/tcp_client.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <assert.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,38 +7,115 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-static const char* payload = "hello";
+static void log_info(const char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+}
 
-static void log_abort(const char *msg) {
+static void log_fatal(const char *msg)
+{
 	int err = errno;
 	fprintf(stderr, "[%d] %s\n", err, msg);
 	abort();
 }
 
-int main() {
-	int fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (fd < 0) {
-		log_abort("failed to construct socket");
+static int32_t read_full(int fd, char *buf, size_t n)
+{
+	while (n > 0) {
+		ssize_t rv = read(fd, buf, n);
+		if (rv <= 0) {
+			return -1;
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+	while (n > 0) {
+		ssize_t rv = write(fd, buf, n);
+		if (rv <= 0) {
+			return -1;
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+int32_t query(int fd, const char *message)
+{
+	uint32_t len = (uint32_t)strlen(message);
+	if (len > k_max_msg) {
+		return -1;
 	}
 
-	struct sockaddr_in addr = {};
+	// send request
+	char wbuf[4 + k_max_msg];
+	memcpy(wbuf, &len, 4);
+	memcpy(&wbuf[4], message, len);
+	int32_t err = write_all(fd, wbuf, 4 + len);
+	if (err) {
+		return err;
+	}
+
+	// read header/len
+	char rbuf[4 + k_max_msg + 1];
+	errno = 0;
+	err = read_full(fd, rbuf, 4);
+	if (err) {
+		log_info(errno == 0 ? "EOF" : "error in reading len");
+		return err;
+	}
+	memcpy(&len, rbuf, 4);
+	if (len > k_max_msg) {
+		log_info("message too long");
+		return -1;
+	}
+
+	// read message
+	err = read_full(fd, &rbuf[4], len);
+	if (err) {
+		log_info("error in reading message");
+		return err;
+	}
+
+	printf("server says: %.*s\n", len, &rbuf[4]);
+	return 0;
+}
+
+int main()
+{
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd < 0) {
+		log_fatal("failed to construct socket");
+	}
+
+	struct sockaddr_in addr = { 0 };
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
 	addr.sin_port = ntohs(1234);
 	int dial = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
 	if (dial) {
-		log_abort("failed to connect");
+		log_fatal("failed to connect");
 	}
 
-	write(fd, payload, strlen(payload));
-
-	char rbuf[64] = {};
-	ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-	if (n < 0) {
-		log_abort("failed to read");
+	int32_t err = query(fd, "hello1");
+	if (err) {
+		goto L_DONE;
+	}
+	err = query(fd, "hello2");
+	if (err) {
+		goto L_DONE;
 	}
 
-	printf("server said: %s\n", rbuf);
+L_DONE:
 	close(fd);
 	return 0;
 }
--- examples/tcp_server.c
+++ examples/tcp_server.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <assert.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,30 +7,85 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-static void log_info(const char *msg) {
+static void log_info(const char *msg)
+{
 	fprintf(stderr, "%s\n", msg);
 }
 
-static void log_fatal(const char *msg) {
+static void log_fatal(const char *msg)
+{
 	int err = errno;
 	fprintf(stderr, "[%d] %s\n", err, msg);
 	abort();
 }
 
-// TODO? maybe return the erroring socket for logging
-static void respond(int connfd) {
-	char rbuf[64] = {0};
-	ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-	if (n < 0) {
-		log_info("read from socket error");
-		return;
+static int32_t read_full(int fd, char *buf, size_t n)
+{
+	while (n > 0) {
+		ssize_t rv = read(fd, buf, n);
+		if (rv <= 0) {
+			if (errno == EINTR) {
+				continue;
+			}
+			return -1;
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
 	}
-	printf("client says %s\n", rbuf);
-	char wbuf[] = "world";
-	write(connfd, wbuf, strlen(wbuf));
+	return 0;
 }
 
-int main() {
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+	while (n > 0) {
+		ssize_t rv = write(fd, buf, n);
+		if (rv <= 0) {
+			return -1;
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+static int32_t respond(int connfd)
+{
+	char rbuf[4 +
+		  k_max_msg]; // message is len (4 chars), then content (4096 chars)
+	errno = 0; // reset
+	int32_t err = read_full(connfd, rbuf, 4);
+	if (err) {
+		log_info(errno == 0 ? "EOF" :
+				      "error in reading len of message");
+		return err;
+	}
+	uint32_t len = 0;
+	memcpy(&len, rbuf, 4); // assumes little endian
+	if (len > k_max_msg) {
+		log_info("len of message too long");
+		return -1;
+	}
+	err = read_full(connfd, &rbuf[4], len);
+	if (err) {
+		log_info("error in reading message");
+		return err;
+	}
+	printf("client says: %.*s\n", len, &rbuf[4]);
+
+	const char reply[] = "world";
+	char wbuf[4 + sizeof(reply)];
+	len = (uint32_t)strlen(reply);
+	memcpy(wbuf, &len, 4);
+	memcpy(&wbuf[4], reply, len);
+	return write_all(connfd, wbuf, 4 + len);
+}
+
+int main()
+{
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (fd < 0) {
 		log_fatal("failed to construct socket");
@@ -38,7 +94,7 @@ int main() {
 	int optval = 1;
 	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
-	struct sockaddr_in addr = {0};
+	struct sockaddr_in addr = { 0 };
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = ntohl(0);
 	addr.sin_port = ntohs(1234);
@@ -54,14 +110,20 @@ int main() {
 
 	// TODO event loop
 	while (1) {
-		struct sockaddr_in client_addr = {0};
+		struct sockaddr_in client_addr = { 0 };
 		socklen_t socklen = sizeof(client_addr);
-		int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+		int connfd =
+			accept(fd, (struct sockaddr *)&client_addr, &socklen);
 		if (connfd < 0) {
 			continue;
 		}
 
-		respond(connfd);
+		while (1) {
+			int32_t err = respond(connfd);
+			if (err) {
+				break;
+			}
+		}
 		close(connfd);
 	}
 
