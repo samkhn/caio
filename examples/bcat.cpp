953e3fdc56a2c1619922a421e63123ed60d3d291
Samiur Khan <samiurkh1n@gmail.com>
2025-03-10 16:11:30 -0400

    add cat and seek binary

---
+++ examples/bcat.cpp
@@ -0,0 +1,47 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <array>
+#include <iostream>
+#include <string_view>
+
+static constexpr std::string_view kHelp = R"(
+bcat - basic cat clone
+
+Usage:
+  bcat [filename]
+
+Arguments:
+  filename - (optional) path to a file
+
+)";
+
+static constexpr ssize_t kDefaultBufferSize = 4096;
+
+int main(int argc, char **argv) {
+  int fd;
+  bool is_file = false;
+  if (argc > 2) {
+    std::cerr << kHelp << '\n';
+    return -1;
+  } else if (argc == 2) {
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+      std::cerr << "Could not open file " << argv[1] << '\n';
+      return -1;
+    }
+    is_file = true;
+  } else {
+    fd = 0;  // stdin
+  }
+  std::array<char, kDefaultBufferSize> buffer;
+  ssize_t bytes_read, bytes_written;
+  do {
+    bytes_read = read(fd, buffer.data(), buffer.max_size());
+    if (bytes_read > 0) {
+      bytes_written = write(1, buffer.data(), bytes_read);
+    }
+  } while (bytes_read > 0 && bytes_written == bytes_read);
+  if (is_file) close(fd);
+  return 0;
+}
---
+++ examples/seek.cpp
@@ -0,0 +1,59 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <array>
+#include <iostream>
+#include <string_view>
+
+static constexpr std::string_view kHelp = R"(
+seek - read count bytes of file at an offset
+
+Usage:
+  seek [filename] [position] [bytes]
+
+Arguments:
+  filename - path to a file
+  offset   - non-negative integer offset into a file to begin reading from
+  bytes    - non-negative integer count of bytes to read
+
+)";
+
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    std::cerr << kHelp << '\n';
+    return -1;
+  }
+  int fd = open(argv[1], O_RDONLY);
+  if (fd < 0) {
+    std::cerr << "could not open file " << argv[1] << '\n';
+    return -1;
+  }
+  int position = atoi(argv[2]);
+  if (position < 0) {
+    std::cerr << "position must be >= 0\n";
+    return -1;
+  }
+  int bytes = atoi(argv[3]);
+  if (bytes <= 0) {
+    std::cerr << "bytes must be > 0\n";
+    return -1;
+  }
+  off_t seek_result = lseek(fd, position, SEEK_SET);
+  if (seek_result == -1) {
+    std::cerr << "could not seek to " << position << '\n';
+    return -1;
+  }
+  char *buffer = (char *)malloc(bytes * sizeof(char));
+  ssize_t bytes_read;
+  if ((bytes_read = read(fd, buffer, bytes))) {
+    write(1, buffer, bytes_read);
+    std::cout << '\n';
+  } else {
+    std::cerr << "could not read " << bytes << " bytes at position " << position
+              << '\n';
+    return -1;
+  }
+  free(buffer);
+  close(fd);
+  return 0;
+}
